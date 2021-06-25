e49f7faa55d68ec1dc248347b64008a2611a153a
Fiona <fionalim1299@gmail.com>
2021-06-25 19:59:29 +0700

    Initialize web-perf-01

---
+++ src/pages/ProductList/index.js
@@ -0,0 +1,66 @@
+import { useMemo } from 'react';
+import { gql, useQuery } from '@apollo/client';
+import ProductCard from '../../components/ProductCard';
+import Loader from '../../components/ProductCard/Loader';
+import './styles.css';
+
+const ProductListQuery = gql`
+  query GetProductLists($offset: Int, $limit: Int) {
+    ProductLists(offset: $offset, limit: $limit) {
+      data {
+        product_id
+        product_price
+        product_price_format
+        product_name
+        rating
+        product_image
+        product_slug
+      }
+      offset
+      hasNext
+    }
+  }
+`;
+
+const ProductList = () => {
+  const { data = {}, loading } = useQuery(ProductListQuery, {
+    variables: {
+      offset: 0,
+      limit: 10,
+    },
+    // default is true, so you can remove this actually
+    // just to make you aware of this
+    ssr: true,
+  });
+
+  const productList = useMemo(() => {
+    if (data.ProductLists) {
+      return (data.ProductLists.data || []).map((item) => ({
+        price: item.product_price_format,
+        imageUrl: item.product_image,
+        name: item.product_name,
+        slug: item.product_slug,
+        id: item.product_id,
+        rating: item.rating,
+      }));
+    }
+
+    return [];
+  }, [data]);
+
+  return loading ? (
+    <div className="product-list">
+      {[1, 2, 3, 4].map((idx) => (
+        <Loader key={idx} />
+      ))}
+    </div>
+  ) : (
+    <div className="product-list">
+      {productList.map((product) => (
+        <ProductCard to={`${product.id}/${product.slug}`} data={product} key={product.id} />
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
